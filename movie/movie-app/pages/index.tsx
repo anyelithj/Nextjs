2feb6e93e8a24be3a39fb08c53954b9228554bbe
Anyelith Jacobo <anyelithj@misena.edu.co>
2022-10-23 17:51:48 -0500

    render api movie Netflix commit

--- movie/movie-app/pages/index.tsx
+++ movie/movie-app/pages/index.tsx
@@ -1,24 +1,54 @@
-import type { NextPage } from 'next'
+// import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import requests from '../utils/requests'
+import { Movie } from '../typings'
+import Row from '../components/Row'
 
-const Home: NextPage = () => {
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}:Props) => {
+  console.log(netflixOriginals)
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
        {/**Header */}
-       <main>
+       <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
         {/*Banner*/}
-        <Banner/>
-        <section>
-        {/**Row*/}
+        <Banner netflixOriginals={netflixOriginals}/>
+        <section className='md:space-y-24'>
+        <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List Component */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
        </main>
        {/**modal */}
