1896f05f69cda3f9aacddd17ef73f2d705900b0e
anyelithj <anyelithjacobo@gmail.com>
2025-02-20 22:54:19 -0500

    multi-vender

---
+++ e-commerce_multi_vendor_sass/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+export default function Home() {
+  return (
+    <div className={cn("text-red-900", "uppercase")}>
+      <Button variant="destructive" size="sm" >Happy new year</Button>
+    </div>
+    //  <div className="text-red-900 uppercase">
+    //  Happy new year
+    // </div>
+  );
+}
